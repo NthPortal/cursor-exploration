1c6a740cc56cde1275e2bd2b3f00652513034a41
NthPortal <nthportal@gmail.com>
2020-12-03 02:55:39 -0500

    Add docs for Cursor and ReverseCursor

--- src/main/scala/scala/collection/Cursor.scala
+++ src/main/scala/scala/collection/Cursor.scala
@@ -1,13 +1,55 @@
 package scala.collection
 
+/**
+ * A utility for traversing a collection with a cursor that
+ * points to or "hovers over" each element one at a time.
+ *
+ * At any given time, a cursor is said to be either "over an element"
+ * or "over nothing"; likewise, when a cursor is over an element, that
+ * element is said to be "under the cursor". When using a cursor to
+ * traverse a collection, it starts over nothing, and the first call
+ * to `advance()` places the cursor over the first element of the collection
+ * (assuming it is non-empty). Subsequent calls to `advance()` put the
+ * cursor over successive elements in the collection. A method call (on a
+ * `Cursor` supporting such functionality) that removes the `current`
+ * element under the cursor from the underlying collection places the
+ * cursor over nothing in a "gap" between the prior and subsequent elements
+ * of the collection, where the removed element used to be. When a cursor is
+ * over the last element of a collection, a subsequent call to `advance()`
+ * places the cursor over nothing, past the last element of the collection.
+ *
+ * @tparam A the type of elements this cursor traverses
+ */
 trait Cursor[+A] extends IterableOnce[A] {
+
+  /**
+   * Advances this cursor and places it over the next element,
+   * or over nothing if there are no subsequent elements to traverse.
+   *
+   * @return `true` if the cursor was placed over the next
+   *         element; `false` otherwise
+   */
   def advance(): Boolean
 
+  /**
+   * @throws scala.NoSuchElementException if this cursor is not over
+   *                                      an element
+   * @return the current element under this cursor
+   */
   @throws[NoSuchElementException]
   def current: A
 
+  /**
+   * @return an [[Iterator]] over the remaining elements for this
+   *         cursor to traverse, excluding the element currently under
+   *         the cursor (if any)
+   */
   override def iterator: Iterator[A] = new Cursor.WrappedCursor[A](this)
 
+  /**
+   * @return the number of elements remaining for this cursor to traverse,
+   *         excluding the element currently under the cursor (if any)
+   */
   def size: Int = {
     var res = 0
     while (advance()) res += 1
@@ -19,7 +61,7 @@ object Cursor {
   // sentinel indicating that a cursor is not over an element
   private[collection] final val overNothing = new AnyRef
 
-  // may be needed by more advanced mutable Cursors
+  // TODO: create a single empty cursor implementation with all the traits/mixins
   private[collection] trait Empty extends Cursor[Nothing] {
     def advance() = false
     @throws[NoSuchElementException]
@@ -29,8 +71,13 @@ object Cursor {
 
   private[this] val _empty: Cursor[Nothing] = new AbstractCursor[Nothing] with Empty
 
+  /** @return an empty cursor over no elements */
   @inline final def empty[A]: Cursor[A] = _empty
 
+  /**
+   * @return a `Cursor` that iterates over the remaining elements
+   *         of the given [[Iterator]]
+   */
   def fromIterator[A](iterator: Iterator[A]): Cursor[A] =
     iterator match {
       case iterator: WrappedCursor[A] => iterator.cursor
@@ -70,7 +117,7 @@ object Cursor {
 
     def advance(): Boolean = {
       val hasNext = it.hasNext
-      if (hasNext) cur = it.next().asInstanceOf[AnyRef]
+      cur = if (hasNext) it.next().asInstanceOf[AnyRef] else overNothing // reset so we don't leak memory
       hasNext
     }
 
--- src/main/scala/scala/collection/ReverseCursor.scala
+++ src/main/scala/scala/collection/ReverseCursor.scala
@@ -1,6 +1,25 @@
 package scala.collection
 
+/**
+ * A [[Cursor]] that can also traverse a collection in reverse,
+ * and can change the direction of traversal at any time.
+ *
+ * When a reverse cursor is over the first element of a collection, a
+ * subsequent call to `retreat()` places the cursor over nothing, before
+ * the first element of the collection.
+ *
+ * @tparam A the type of elements this cursor traverses
+ */
 trait ReverseCursor[+A] extends Cursor[A] {
+
+  /**
+   * Retreats this cursor and places it over the previous element,
+   * or over nothing if there are no previous elements to traverse
+   * in reverse.
+   *
+   * @return `true` if the cursor was placed over the previous
+   *         element; `false` otherwise
+   */
   def retreat(): Boolean
 }
 
@@ -11,5 +30,6 @@ object ReverseCursor {
 
   private[this] val _empty: ReverseCursor[Nothing] = new AbstractCursor[Nothing] with Empty
 
+  /** @return an empty reverse cursor over no elements */
   @inline final def empty[A]: ReverseCursor[A] = _empty
 }
